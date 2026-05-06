4b3c48c5e068c0864901535b5f52ce349e0c2200
Romain1804 <romaincharles42@gmail.com>
2026-05-06 11:32:02 +0200

    js3

--- script les conditions.js
+++ script les conditions.js
@@ -146,16 +146,104 @@ console.log(ticket)
 
 function tricolore() {
     let color = prompt("quel est la couleur du feu tricolor")
+    //si la couleur = rouge -> je m'arrete
     if (color === "rouge") {
         console.log("je m'arrête")
+        //si la couleur = orange -> je freine
     } else if (color === "orange") {
         console.log("je freine")
+        //si la couleur = vert -> je passe
     } else if (color === "vert") {
         console.log("je passe!")
+        //sinon danger feu defectueux 
     } else {
         console.log("danger! feu defectueux")
     }
 
 }
 
+tricolore()
+
+//correction 
+
+/* 
+demander a l'utilisateur de quelle couleur est le feu
+role : demander a l'utilisateur de quelle couleur est le feu
+parametre : non
+return : la reponse de l'utilisateur ( rouge, vert, orange ou autre)
+bien penser a donner le nom de l'utilisation ex: demander couleur) pour les fonction 
+
+separe la fonction prompt et la fonction afficher message 
+role afficher une indication de conduite
+parametre : couleur du feu 
+return rien
+
+fonction afficher couleur 
+
+3eme fonction
+role appeler  demander couleur puis affiche info
+parametre 
+return
+
+function lancerfonction () { 
+let coileur = demander couleur ()
+afficher infoconduite ( couleur)}
+*/ 
+
+//exo2 bulletin de note 
+
+// role calculer et retourner la moyenne generale  
+// parametre : moyenne des matiere 
+// retour: la moyenne general 
+
+function moyeng(math,français,histoire,geo,svt,physique){
+    return (math+français+histoire+geo+svt+physique)/6
+}
+// fonction calcul et retour de la moyenne g
+ let mg =moyeng(18,15.2,18.9,19,17,17.9)
+
+console.log(mg) 
+
+// créer un commentaire en fonction de la moyenne g  
+// parametre : moyeng
+// retour: afficher le commentaire selon la moyenne g
+
+function commentaire(moyeng){
+    if(moyeng>0 && moyeng==10){
+        return "il faut travailler plus"
+    }else if (moyeng>10 && moyeng<=13){
+        return"poursuivez vos effort"
+    }else if (moyeng>13 && moyeng<=15){
+        return"bon travail"
+    }else if (moyeng>15 && moyeng<=17){
+        return "bravo!"
+    }else{return"felicitation!"}
+}
+
+let commentair= commentaire(mg)
+
+console.log(commentair)
+
+//role afficher la moyenneg et le commentaire 
+// parametre : moyeng et commentair
+// return rien car elle affiche
+
+function bulletin(commentaireEleve,moyenneGeneral){
+   console.log(`la moyenne general de l'eleve est ${moyenneGeneral} et le commentaire est : ${commentaireEleve}`)
+}
+
+bulletin(commentair,mg)
+
+// role : jouer le scenario -> calcul moyenne, commentaire, bulletin
+// parametre : notes 
+// return rien car elle affiche
+
+function scenario(a,b,c,d,e,f,prenom){
+    let moyg =moyeng(18,15.2,18.9,19,17,17.9)
+    let comenteir=commentaire(moyeng)
+
+    bulletin(moyg,comenteir,prenom)
+} 
+
+scenario(18,15.2,18.9,19,17,17.9,"marie")
 
