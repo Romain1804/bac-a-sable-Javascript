ff37dbab781220a2ff734b26e95a39a8b65cb85d
Romain1804 <romaincharles42@gmail.com>
2026-05-12 17:05:12 +0200

    js

--- manipDOM.js
+++ manipDOM.js
@@ -1,3 +1,4 @@
+/* 
 // trois maniere de selectionner des élements dans mon document c'est a dire dans mon html 
 
 //le body
@@ -31,3 +32,75 @@ paragrapheamettreEnrouge.classList.toggle("bgyellow")
 function ajouteunparagraphe(){
     document.querySelector("#grosseboite").innerHTML+="<p> lorem ipsum dolor lorys </p>"
 }
+
+//exo 1 h1 style
+
+//document.querySelector('#titre').style.color="red"
+
+//exo 2 interrupteur 
+
+//role permettre d'afficher puis de cacher en cliquant dessus ( un element dont l'id est en parametre)
+//parametre l'id donné
+//return rien elle affiche
+
+let masquer = document.querySelector("#ptiteboite")
+masquer.classList.toggle("display")
+
+//exo 3 
+
+//role remplir le cube de couleur 
+//parametre rien
+//return rien 
+
+//colorier la boite en jaune 
+function couleur(){
+  
+}
+
+
+//faire pareil pour les deux autre boites 
+
+
+// mettre la display pour que il y ait que un cube sur 3 qui soit colorier 
+
+// mettre la fonction cliqué */
+
+let boites =document.querySelectorAll(".card")
+
+console.log(boites)
+
+//je parcours mon tableaux de div
+boites.forEach(boite => {
+    boite.addEventListener("click", function(){
+        reset()
+        // j'ai la obite sur laquelle j'ai cliqué
+        console.log(boite)
+        boite.classList.toggle("bgyellow")
+    })
+});
+
+//role reset la classe bgyellow a toute les div qui ont la classe card 
+//parametre rien
+//return rien
+
+function reset(){
+    document.querySelectorAll(".boite")
+    boites.forEach(boite => {
+        boite.classList.remove("bgyellow")
+        
+    });
+}
+
+//exo 4
+
+let nombreDeClics=0
+
+function comptage(){
+    nombreDeClics++
+}
+
+comptage()
+
+document.getElementById("#boutonClic").addEventListener("click", comptage);
+
+
